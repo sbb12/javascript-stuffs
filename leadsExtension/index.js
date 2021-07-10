bfea98aa50c0ff57eb2c83c9a303508e97c121c3
sbb12 <sercansokmen0@gmail.com>
2021-07-10 17:48:33 +0100

    Leads extension
    a leads extension which saves inputs or the current tab you are viewing in a list so that you can save potential leads (people you might want to contact etc)
    
    double click delete all to delete the list.

--- leadsExtension/index.js
+++ leadsExtension/index.js
@@ -3,20 +3,53 @@ function saveLead() {
 }
 
 const inputEl = document.getElementById("input-el");
-const saveBtn = document.getElementById("save-btn");
 const leadsEl = document.getElementById("leads-list-el");
+const saveBtn = document.getElementById("save-btn");
+const tabBtn = document.getElementById("save-tab-btn");
+const deleteBtn = document.getElementById("delete-btn");
 
-let leadsList = "";
+if (localStorage.getItem("leadsList")) {
+  leadsList = localStorage.getItem("leadsList");
+  updateLeadsDisplay();
+} else {
+  leadsList = [];
+  localStorage.setItem("leadsList", JSON.stringify(leadsList));
+}
 
 saveBtn.addEventListener("click", function () {
   let lead = inputEl.value;
+  addToList(lead);
   inputEl.value = "";
-  leadsList += `
-  <li>
-    <a href="${lead}" target="_blank"> 
-    ${lead}
-    </a>
-  </li>`;
-
-  leadsEl.innerHTML = leadsList;
+  updateLeadsDisplay();
 });
+
+tabBtn.addEventListener("click", function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    tabUrl = tabs[0].url;
+  });
+  addToList(tabUrl);
+  inputEl.value = "";
+  updateLeadsDisplay();
+});
+
+deleteBtn.addEventListener("dblclick", function deleteList() {
+  deleteBtn.style;
+  localStorage.setItem("leadsList", [JSON.stringify([])]);
+  updateLeadsDisplay();
+});
+
+function addToList(itemToAdd) {
+  leadsList = localStorage.getItem("leadsList");
+  leadsList = JSON.parse(leadsList);
+  leadsList.push(itemToAdd);
+  leadsList = JSON.stringify(leadsList);
+  localStorage.setItem("leadsList", leadsList);
+}
+
+function updateLeadsDisplay() {
+  leadsEl.innerHTML = "";
+  leadsList = JSON.parse(leadsList);
+  for (let lead of leadsList) {
+    leadsEl.innerHTML += `<li><a href="${lead}" target="_blank">${lead}</a></li>`;
+  }
+}
